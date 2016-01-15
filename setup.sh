32f35f59d87cbadf49fe4dd7abdcae43f63ad4c5
kurozumi <kurozumi@internetmarketing.co.jp>
2016-01-15 18:05:12 +0900

    .bash_profileを反映させる

--- setup.sh
+++ setup.sh
@@ -6,20 +6,13 @@ echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
 echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
 
-exec $SHELL
+source ~/.bash_profile
 
 pyenv install 2.7.8
-
 pyenv rehash
-
 pyenv global 2.7.8
 
 pip install -r piplist.txt
-
-#git clone git://github/com/ariya/phantomjs.git
-#cd phantomjs
-#checkout 2.0
-#./build.py
 
 wget https://phantomjs.googlecode.com/files/phantomjs-1.9.2-linux-x86_64.tar.bz2
 tar jxfv phantomjs-1.9.2-linux-x86_64.tar.bz2
