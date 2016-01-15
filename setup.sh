ef9f3bcbeeb44ac1b3844526778223aecb987afb
kurozumi <kurozumi@internetmarketing.co.jp>
2016-01-15 12:34:36 +0900

    first commit

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
+
+exec $SHELL
+
+pyenv install 2.7.8
+
+pyenv rehash
+
+pyenv global 2.7.8
+
+pip install -r piplist.txt
+
+
